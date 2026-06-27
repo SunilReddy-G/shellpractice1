3487c02996bbfa5dc8f7ed3e202243a9fad02ef7
sunilreddy.g <sunilsmec@gmail.com>
2026-06-27 18:00:31 +0530

    ansible

---
+++ Commandlinevar.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+PACKAGE="httpd wget unzip"
+SVC="httpd"
+#URL="https://www.tooplate.com/zip-templates/2160_exhibit_studio.zip"
+#ART_NAME="2160_exhibit_studio"
+TEMPDIR=" /tmp/webfiles"
+
+# install dependencies
+echo "#####################################"
+echo "Install packages"
+echo "#####################################"
+sudo dnf install $PACKAGE -y > /dev/null
+echo
+
+# start and enabled service
+echo "#####################################"
+echo "Start & enable HTTPD services"
+echo "#####################################"
+sudo systemctl start $SVC
+sudo systemctl enable $SVC
+echo
+
+# Creating temp directory
+echo "#####################################"
+echo "Starting Artifact Deployment"
+echo "#####################################"
+mkdir -p $TEMPDIR
+cd $TEMPDIR
+echo
+
+ 
+wget $1 > /dev/null
+unzip $2.zip > /dev/null
+sudo cp -r $2/* /var/www/html/
+echo
+
+# Bounce service
+echo "#####################################"
+echo "Restarting HTTPD Services"
+echo "#####################################"
+sudo systemctl restart $SVC
+echo
+
+# clean up
+echo "#####################################"
+echo "Remove temporary files"
+echo "#####################################"
+rm -rf $TEMPDIR
+echo
+
+sudo systemctl status $SVC
+
+ls /var/www/html/
