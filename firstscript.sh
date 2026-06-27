8b75c8dafb43eaa1d58c036f086a74aea0c533f2
sunilreddy.g <sunilsmec@gmail.com>
2026-06-27 11:47:06 +0530

    ansible

--- firstscript.sh
+++ firstscript.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-# this script prints system info
+### this script prints system info ###
 
 echo "This is my fist Bashscript"
 echo
+
+# checking the system uptime
 echo "###################################"
 echo "this is uptime of the script"
 uptime
 
+# Memory utilization
 echo "###################################"
 echo "Memory utilization"
 free -m
 
+# disk utilization
 echo "###################################"
 echo " Disk utilization"
 df -h
