8dda6dad5bc258cd542b2eca053552892db43959
sunilreddy.g <sunilsmec@gmail.com>
2026-07-01 05:24:40 +0530

    practice

---
+++ conversation.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+
+
+
+echo "Suresh :: Hi ramesh how are you?"
+echo "Ramesh :: I am fine thank you and how about you?"
+echo "Suresh :: I am good thank you to"
+echo "Ramesh :: what are you learning Suresh?"
+echo "Suresh :: I am learning DevOps"
