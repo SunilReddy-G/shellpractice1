b6f04db87f6e0e2fca1d385d4654ebf8082d280b
sunilreddy.g <sunilsmec@gmail.com>
2026-07-01 05:35:05 +0530

    Name library added

--- conversation.sh
+++ conversation.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+PERSON1=Suresh
+PERSON2=Ramesh
 
-
-echo "Suresh :: Hi ramesh how are you?"
-echo "Ramesh :: I am fine thank you and how about you?"
-echo "Suresh :: I am good thank you to"
-echo "Ramesh :: what are you learning Suresh?"
-echo "Suresh :: I am learning DevOps"
+echo "$PERSON1 :: Hi $PERSON2 how are you?"
+echo "$PERSON2 :: I am fine thank you and how about you?"
+echo "$PERSON1 :: I am good thank you to"
+echo "$PERSON2 :: what are you learning $PERSON1?"
+echo "$PERSON1 :: I am learning DevOps"
