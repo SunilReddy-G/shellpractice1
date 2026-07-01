0408955c9a94775ff8fef86dd9a3cefe76a6bed6
sunilreddy.g <sunilsmec@gmail.com>
2026-07-01 05:38:20 +0530

    Commandline variable declared

---
+++ cmdlinevarsconv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: Hi $PERSON2 how are you?"
+echo "$PERSON2 :: I am fine thank you and how about you?"
+echo "$PERSON1 :: I am good thank you to"
+echo "$PERSON2 :: what are you learning $PERSON1?"
+echo "$PERSON1 :: I am learning DevOps"
